dac2c77e5aaf4a4c7bacda884ce9f750ea9fcd5e
Anderson <alvesmonteiroanderson@gmail.com>
2025-04-21 23:38:27 -0300

    FEAT: Item List w/o reactivity

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import AppHeader from './components/AppHeader.vue'
+import TodoList from './components/TodoList.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <AppHeader />
+  <TodoList />
 </template>
 
 <style scoped>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import ListItem from './ListItem.vue'
+import { ref, Ref } from 'vue'
+
+type Item = {
+  title: string
+  checked?: boolean
+}
+
+const listItems: Ref<Item[]> = ref([
+  { title: 'Make a todo list app', checked: true },
+  { title: 'Predict the Weather', checked: false },
+  { title: 'Play some tunes', checked: false },
+  { title: 'Make a todo list app', checked: false },
+  { title: 'Make a todo list app', checked: false },
+  { title: 'Pump some iron', checked: false },
+  { title: 'Track my expenses', checked: false },
+  { title: 'Organize a game night', checked: false },
+  { title: 'Learn a new language', checked: false },
+  { title: 'Publish my work', checked: false },
+])
+</script>
+
+<template>
+  <ul>
+    <li :key="key" v-for="(item, key) in listItems">
+      <ListItem :isChecked="item.checked">{{ item.title }}</ListItem>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0.4rem;
+}
+</style>
