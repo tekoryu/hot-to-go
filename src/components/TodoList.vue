a0e2a73454df31daebff7e790944f0470499de43
Anderson <alvesmonteiroanderson@gmail.com>
2025-04-26 15:54:03 -0300

    Make the code equal to the lesson

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -8,26 +8,23 @@ type Item = {
   checked?: boolean
 }
 
-// The listItems array is a reactive reference that contains the list of items.
-// Each item is an object with a title and a checked property.
-// The checked property is optional and defaults to false.
-const listItems: Ref<Item[]> = ref([
-  { title: 'Make a todo list app', checked: true },
-  { title: 'Predict the Weather', checked: false },
-  { title: 'Play some tunes', checked: false },
-  { title: 'Pump some iron', checked: false },
-  { title: 'Track my expenses', checked: false },
-  { title: 'Organize a game night', checked: false },
-  { title: 'Learn a new language', checked: false },
-  { title: 'Publish my work', checked: false },
-])
+const storageItems: Ref<Item[]> = ref([])
 
-// The updateItem function takes an item as an argument and finds it in the listItems array.
-// If the item is found, it toggles its checked property using the toggleItemChecked function.
+// const listItems: Ref<Item[]> = ref([
+//   { title: 'Make a todo list app', checked: true },
+//   { title: 'Predict the Weather', checked: false },
+//   { title: 'Play some tunes', checked: false },
+//   { title: 'Pump some iron', checked: false },
+//   { title: 'Track my expenses', checked: false },
+//   { title: 'Organize a game night', checked: false },
+//   { title: 'Learn a new language', checked: false },
+//   { title: 'Publish my work', checked: false },
+// ])
+
 const updateItem = (item: Item): void => {
-  const updateItem = findItemInList(item)
-  if (updateItem) {
-    toggleItemChecked(updateItem)
+  const updatedItem = findItemInList(item)
+  if (updatedItem) {
+    toggleItemChecked(updatedItem)
     setToStorage(storageItems.value) // Store the updated list in local storage
   }
 }
@@ -42,27 +39,25 @@ const toggleItemChecked = (item: Item): void => {
   }
 }
 
-// The sortedList computed property sorts the listItems array based on the checked property.
 const sortedList = computed(() =>
   [...storageItems.value].sort((a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0)),
 )
 
-// Preserving changes to the list
-const setToStorage = (items: Item[]) => {
+const setToStorage = (items: Item[]): void => {
   localStorage.setItem('list-items', JSON.stringify(items)) // list-items is the same as listItems
+  console.log('List items saved to local storage:', JSON.stringify(items))
 }
 
 const getFromStorage = (): Item[] | [] => {
   const stored = localStorage.getItem('list-items')
   if (stored) {
+    console.log('List items retrieved from local storage:', stored)
     return JSON.parse(stored)
   }
   return []
 }
-//Var declaration:  Reactive Type = Creates empty reactive reference
-const storageItems: Ref<Item[]> = ref([])
 
-const initListItems = () => {
+const initListItems = (): void => {
   if (storageItems.value?.length === 0) {
     const listItems = [
       { title: 'Make a todo list app', checked: true },
@@ -78,12 +73,12 @@ const initListItems = () => {
     setToStorage(listItems) // Store the initial list in local storage
     storageItems.value = listItems // Assign the initial list to the storageItems reference, that is reactive
   }
-
-  onMounted(() => {
-    initListItems() // Initialize the list items when the component is mounted
-    storageItems.value = getFromStorage() // Get the list from local storage
-  })
 }
+
+onMounted(() => {
+  initListItems() // Initialize the list items when the component is mounted
+  storageItems.value = getFromStorage() // Get the list from local storage
+})
 </script>
 
 <template>
